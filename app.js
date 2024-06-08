cddf3470ab532d2e9f1f5d4cccec53c5f513a7e8
EricaMoua <erica.moua568@gmail.com>
2024-06-07 22:36:44 -0400

    email valid

--- app.js
+++ app.js
@@ -21,8 +21,10 @@
 // 2 .Create getNotes function that returns "Your notes.."
 // 3. Export getNotes function 
 // 4. from app.js load in and call the function printing message to console
-
+const validator = require('validator')
 const getNotes = require('./notes')
 const msg = getNotes()
 
 console.log(msg);
+
+console.log(validator.isURL('https/mead.io'));
